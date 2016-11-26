d408e4720e023448f74dbd06d05a8034150c10de
Dulio Denis <dulio.denis@yahoo.com>
2016-11-26 12:53:18 -0500

    Added UIPickerView for tea selection and added connections

--- Tea Time/Tea Time/TeaTimeViewController.swift
+++ Tea Time/Tea Time/TeaTimeViewController.swift
@@ -10,4 +10,17 @@ import UIKit
 
 class TeaTimeViewController: UIViewController {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var teaPickerView: UIPickerView!
+    
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetTimer(_ sender: UIButton) {
+        
+    }
+    
 }
